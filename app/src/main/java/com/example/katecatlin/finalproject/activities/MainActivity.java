7ce60dec046cc3b820d9657cdc368c1914a77d42
Kate Catlin <kate.catlin@detroitlabs.com>
2014-11-25 23:44:00 -0500

    Adding XML for detailed view of Artist, definitely room for prettying up

--- app/src/main/java/com/example/katecatlin/finalproject/activities/MainActivity.java
+++ app/src/main/java/com/example/katecatlin/finalproject/activities/MainActivity.java
@@ -4,8 +4,6 @@ import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.katecatlin.finalproject.R;
 import com.example.katecatlin.finalproject.fragments.ConcertListFragment;
@@ -16,7 +14,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_layout);
+        setContentView(R.layout.activity_main);
 
         FragmentManager fragmentManager = getFragmentManager();
         Fragment concertListFragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
