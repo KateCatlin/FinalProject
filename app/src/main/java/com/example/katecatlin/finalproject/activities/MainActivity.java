57a1055684ae14343575f45a1a18f693dd21fac4
Kate Catlin <kate.catlin@detroitlabs.com>
2014-11-26 08:57:28 -0800

    Adding fragment and backstack management to MainActivity

--- app/src/main/java/com/example/katecatlin/finalproject/activities/MainActivity.java
+++ app/src/main/java/com/example/katecatlin/finalproject/activities/MainActivity.java
@@ -3,28 +3,52 @@ package com.example.katecatlin.finalproject.activities;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 
 import com.example.katecatlin.finalproject.R;
 import com.example.katecatlin.finalproject.fragments.ConcertListFragment;
+import com.example.katecatlin.finalproject.interfaces.FragmentController;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements FragmentController {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fragmentManager = getFragmentManager();
-        Fragment concertListFragment = fragmentManager.findFragmentById(R.id.fragmentContainer);
+        loadConcertListFragment();
+    }
 
-        if (concertListFragment == null) {
-            concertListFragment = ConcertListFragment.newInstance();
-            fragmentManager.beginTransaction()
-                    .add(R.id.fragmentContainer, concertListFragment )
-                    .commit();
+    @Override
+    public void onBackPressed() {
+        if (getFragmentManager().getBackStackEntryCount() != 0) {
+            getFragmentManager().popBackStack();
+        } else {
+            super.onBackPressed();
         }
     }
 
+    @Override
+    public void changeFragment(Fragment fragment, boolean addToBackstack) {
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+
+        if (addToBackstack) {
+            fragmentTransaction.addToBackStack(null);
+        }
+
+        fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+        fragmentTransaction.commit();
+    }
+
+    private void loadConcertListFragment () {
+
+        ConcertListFragment concertListFragment = ConcertListFragment.newInstance();
+
+        getFragmentManager().beginTransaction()
+                .replace(R.id.fragmentContainer, concertListFragment)
+                .commit();
+
+    }
 }
---
+++ app/src/main/java/com/example/katecatlin/finalproject/interfaces/FragmentController.java
@@ -0,0 +1,13 @@
+package com.example.katecatlin.finalproject.interfaces;
+
+import android.app.Fragment;
+
+/**
+ * Created by katecatlin on 11/26/14.
+ */
+public interface FragmentController {
+
+    public void changeFragment(Fragment fragment, boolean addToBackstack);
+
+}
+
