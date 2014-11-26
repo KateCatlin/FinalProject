3871c9ef9a611e3a58fa6ea756843bda716d5bc6
Kate Catlin <kate.catlin@detroitlabs.com>
2014-11-26 09:34:39 -0800

    renaming textviews in row_concert_listings

--- app/src/main/java/com/example/katecatlin/finalproject/adapters/ConcertListAdapter.java
+++ app/src/main/java/com/example/katecatlin/finalproject/adapters/ConcertListAdapter.java
@@ -1,6 +1,9 @@
 package com.example.katecatlin.finalproject.adapters;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
 import com.example.katecatlin.finalproject.R;
@@ -13,6 +16,35 @@ public class ConcertListAdapter extends ArrayAdapter<ConcertModel> {
 
     public ConcertListAdapter (Context context) {
         super(context, R.layout.rows_concert_listings);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.rows_concert_listings, parent, false);
+
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        ConcertModel concertModel = getItem(position);
+
+        viewHolder.
+
+
+        return convertView;
+    }
+
+    private static class ViewHolder {
+
+        public ViewHolder(View rootView) {
+
+        }
     }
 
 }
