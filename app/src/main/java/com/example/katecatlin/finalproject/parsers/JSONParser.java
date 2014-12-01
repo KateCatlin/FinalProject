47b643e9aa1f6b13c96db3b6c6f8777169f47179
Kate Catlin <kate.catlin@detroitlabs.com>
2014-12-01 10:38:37 -0500

    Adding untested JSON Parser to code

--- app/src/main/java/com/example/katecatlin/finalproject/parsers/JSONParser.java
+++ app/src/main/java/com/example/katecatlin/finalproject/parsers/JSONParser.java
@@ -2,6 +2,7 @@ package com.example.katecatlin.finalproject.parsers;
 
 import com.example.katecatlin.finalproject.models.ConcertModel;
 
+import org.joda.time.DateTime;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -39,9 +40,47 @@ public class JSONParser {
 
     }
 
-    public static ConcertModel createConcertModelFromJsonInfo (JSONObject newConcertModel) {
-
+    private static ConcertModel createConcertModelFromJsonInfo (JSONObject concertsJsonObject) throws JSONException {
+        String venue, address, city, zipCode, venueURL, artist1, artist2, artist3, ticketUrl;
+        DateTime dateTime;
         ConcertModel concertModel;
+        JSONObject venueObject, artistObject;
+        JSONArray artistArray;
+
+        dateTime = new DateTime(concertsJsonObject.optString("Date"));
+
+        venueObject = concertsJsonObject.getJSONObject("Venue");
+
+        venue = venueObject.optString("Name", "Unknown Venue");
+        address = venueObject.optString("Address", "Unknown Address");
+        city = venueObject.optString("City", "Unknown City");
+        zipCode = venueObject.optString("ZipCode", "Unknown ZIP code");
+        venueURL = venueObject.optString("Url", "No URL");
+
+        artistArray = concertsJsonObject.getJSONArray("Artists");
+
+        //if artists don't exist, we want to display nothing!
+
+        artistObject = artistArray.getJSONObject(0);
+        artist1 = artistObject.optString("Name", "Unknown Artist");
+        artist2 = "";
+        artist3 = "";
+
+        if (artistArray.length() > 1) {
+            artistObject = artistArray.getJSONObject(1);
+            artist2 = artistObject.optString("Name");
+        }
+
+        if (artistArray.length() > 2) {
+            artistObject = artistArray.getJSONObject(2);
+            artist3 = artistObject.optString("Name");
+        }
+
+        ticketUrl = concertsJsonObject.optString("TicketUrl", "Unknown URL");
+
+        concertModel = new ConcertModel(dateTime,venue, address, city, zipCode,
+                venueURL, artist1, artist2, artist3, ticketUrl);
+
 
         return concertModel;
     }
