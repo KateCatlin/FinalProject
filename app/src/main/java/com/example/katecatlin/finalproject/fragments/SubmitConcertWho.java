1114c705416f38666e68a50a4769763e7d8621f7
Kate Catlin <kate.catlin@detroitlabs.com>
2014-12-09 13:30:55 -0500

    Adding initial code to open window to find picture

--- app/src/main/java/com/example/katecatlin/finalproject/fragments/SubmitConcertWho.java
+++ app/src/main/java/com/example/katecatlin/finalproject/fragments/SubmitConcertWho.java
@@ -1,10 +1,15 @@
 package com.example.katecatlin.finalproject.fragments;
 
 import android.app.Fragment;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
@@ -21,7 +26,10 @@ public class SubmitConcertWho extends Fragment {
     private ConcertModel submittedConcert;
     private EditText edit_artist_1, edit_artist_2, edit_artist_3;
     private ImageButton next_button;
+    private Button button_picture_choose;
     static final String SUBMITTED_CONCERT_ENTRY = "SUBMITTED_CONCERT_ENTRY";
+    private static final int SELECT_PICTURE = 1;
+    private String selectedImagePath;
 
 
     public static SubmitConcertWho newInstance(ConcertModel concertModel){
@@ -43,13 +51,29 @@ public class SubmitConcertWho extends Fragment {
         edit_artist_1 = (EditText) view.findViewById(R.id.edit_artist_1);
         edit_artist_2 = (EditText) view.findViewById(R.id.edit_artist_2);
         edit_artist_3 = (EditText) view.findViewById(R.id.edit_artist_3);
+        button_picture_choose = (Button) view.findViewById(R.id.button_picture_choose);
         next_button = (ImageButton) view.findViewById(R.id.button_next);
+
 
         return view;
     }
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+
+        button_picture_choose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // in onCreate or any event where your want the user to
+                // select a file
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent,
+                        "Select Picture"), SELECT_PICTURE);
+            }
+        });
+
         next_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -76,5 +100,36 @@ public class SubmitConcertWho extends Fragment {
         });
     }
 
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_PICTURE) {
+                Uri selectedImageUri = data.getData();
+                selectedImagePath = getPath(selectedImageUri);
+            }
+        }
+    }
 
+
+    /**
+     * helper to retrieve the path of an image URI
+     */
+    public String getPath(Uri uri) {
+        // just some safety built in
+        if( uri == null ) {
+            // TODO perform some logging or show user feedback
+            return null;
+        }
+        // try to retrieve the image from the media store first
+        // this will only work for images selected from gallery
+        String[] projection = { MediaStore.Images.Media.DATA };
+        Cursor cursor = managedQuery(uri, projection, null, null, null);
+        if( cursor != null ){
+            int column_index = cursor
+                    .getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+            cursor.moveToFirst();
+            return cursor.getString(column_index);
+        }
+        // this is our fallback here
+        return uri.getPath();
+    }
 }
